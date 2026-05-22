60584a6d05f6eac46e5ba28721c968db783f0456
Isaac <isaac.l@uteca.edu.mx>
2026-05-21 20:37:51 -0600

    Se agrega scripts

---
+++ Assets/scripts/Player.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+ 
+public class Player : MonoBehaviour
+{
+   [SerializeField]
+   private Transform gunPosition;
+   private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Gun"))
+        {
+           other.GetComponent<Gun>().GrabGun(gunPosition);
+        }
+    }
+}
+ 

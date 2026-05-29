d140649f025e458d941a6a5218227b5d06f75d55
Isaac <isaac.l@uteca.edu.mx>
2026-05-28 20:17:00 -0600

    interfaz de disparo

--- Assets/scripts/InputManager.cs
+++ Assets/scripts/InputManager.cs
@@ -4,11 +4,12 @@ public class InputManager : MonoBehaviour
 {
    public bool LeftButtonPressed { get; private set; }
    public bool LeftButtonHeld { get; private set; }
- 
+   public bool RightButtonPressed { get; private set; }
    private void Update()
     {
         LeftButtonPressed = Input.GetMouseButtonDown(0);
         LeftButtonHeld = Input.GetMouseButton(0);
+        RightButtonPressed = Input.GetMouseButtonDown(1);
     }
 }
  

7ccd42926dd68c50fd6fcc7802715f8610f25721
Isaac <isaac.l@uteca.edu.mx>
2026-06-09 20:35:04 -0600

    Se agrega daño al enemigo y pantalla de daño

--- Assets/scripts/Bullet.cs
+++ Assets/scripts/Bullet.cs
@@ -4,9 +4,20 @@ public class Bullet : MonoBehaviour
 {
     [SerializeField]
     private float speed = 20f;
+    private float damage =10f;
+    public float Damage { set { damage = value; } }
     void Awake()
     {
         GetComponent<Rigidbody>().linearVelocity = transform.forward * speed;
     }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            collision.gameObject.GetComponent<Health>().TakeDamage(damage);
+        }
+        Destroy(gameObject);
+    }
 }
+ 
  
--- Assets/scripts/Player.cs
+++ Assets/scripts/Player.cs
@@ -18,6 +18,7 @@ public class Player : MonoBehaviour
    private void Start()
     {
         onGunDropped?.Invoke();
+        GetComponent<Health>().InitializeHealth();
     }
    private void OnTriggerEnter(Collider other)
     {
@@ -47,6 +48,12 @@ public class Player : MonoBehaviour
         currentGun = null;
         onGunDropped?.Invoke();
     }
+    public void PushBack(Transform enemy, float force)
+    {
+        Vector3 pushDirection = (transform.position - enemy.position).normalized;
+        GetComponent<Rigidbody>().AddForce(pushDirection * force, ForceMode.Impulse);
+    }
 }
+ 
  
  

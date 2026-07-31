4890b43fae8c788ef58026cb7105eb35390ed349
Isaac <isaac.l@uteca.edu.mx>
2026-07-30 20:26:39 -0600

    se agregaron particulas y script de sire

--- Assets/scripts/EnemyWalker.cs
+++ Assets/scripts/EnemyWalker.cs
@@ -1,16 +1,77 @@
 using UnityEngine;
-
-public class EnemyWalker : MonoBehaviour
+using System.Collections;
+ 
+public class EnemyWalker : Enemy
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField]
+    private float speed = 2f;
+    [SerializeField]
+    private float attackRange = 1.5f;
+    [SerializeField]
+    private float damageRange = 2f;
+    [SerializeField]
+    private float attackTime = 1f;
+    private enum State
     {
-        
+        Appearing,
+        Following,
+        Attacking,
+        Death,
     }
-
-    // Update is called once per frame
-    void Update()
+    private State currentState = State.Appearing;
+    private bool IsInRange => Vector3.Distance(transform.position, player.position) <= attackRange;
+    private bool IsInDamageRange => Vector3.Distance(transform.position, player.position) <= damageRange;
+    public override void OnEnable()
     {
-        
+        SoundManager.instance.Play("SireAppear");
+        base.OnEnable();
+        currentState = State.Appearing;
+        StartCoroutine(AppearCoroutine());
+    }
+    private IEnumerator AppearCoroutine()
+    {
+        animator.Play("Appear", 0, 0f);
+        yield return animator.WaitForCurrentAnimation();
+        currentState = State.Following;
+    }
+    private void Update()
+    {
+        if (health.IsDead) return;
+        if (CheckWin()) return;
+        if (currentState == State.Following)
+        {
+            if (IsInRange)
+            {
+                currentState = State.Attacking;
+                StartCoroutine(AttackCoroutine());
+            }
+            else
+            {
+                animator.Play("Run");
+                Vector3 direction = (player.position - transform.position).normalized;
+                transform.position += direction * speed * Time.deltaTime;
+            }
+        }
+        transform.LookAt(player);
+    }
+    private IEnumerator AttackCoroutine()
+    {
+        SoundManager.instance.Play("SireAttack");
+        animator.Play("Attack", 0, 0f);
+        yield return new WaitForSeconds(attackTime);
+        if (IsInDamageRange)
+        {
+            player.GetComponent<Health>().TakeDamage(damage);
+            player.GetComponent<Player>().PushBack(transform, 5f);
+        }
+        yield return animator.WaitForCurrentAnimation();
+        currentState = State.Following;
+    }
+    public override void Die()
+    {
+        currentState = State.Death;
+        rb.isKinematic = true;
+        SoundManager.instance.Play("SireDeath");
+        base.Die();
     }
 }
